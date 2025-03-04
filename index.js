38939cb4f0cdcfd4e788c7a9cd356363ebdc98d0
NattiiZ <nat87972@gmail.com>
2025-03-04 16:57:19 +0700

    .

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const axios = require('axios');
 const path = require("path");
 const bodyParser = require('body-parser');
+const { log } = require("console");
 
 
 const api_port = process.env.API_PORT || 3000;
@@ -24,8 +25,8 @@ app.use(express.static(__dirname + '/public'));
 app.get("/", async (req, res) => {
     try{
         const product = await axios.get(base_url + '/products');
-        const category = await axios.get(base_url + '/category');
         const brand = await axios.get(base_url + '/brands');
+        const category = await axios.get(base_url + '/category');
 
         res.render("customer/home", { products: product.data, category: category.data, brands: brand.data });
     }catch(err){
@@ -34,13 +35,14 @@ app.get("/", async (req, res) => {
     }
 });
 
+
 app.get("/search", async (req, res) => {
     const query = req.query.query;
     
     try {
         const productList = await axios.get(base_url + '/products');
-        const categories = await axios.get(base_url + '/category');
         const brand = await axios.get(base_url + '/brands');
+        const categories = await axios.get(base_url + '/category');
 
         const filteredProducts = productList.data.filter(product =>
             product.name.toLowerCase().includes(query.toLowerCase())
@@ -83,8 +85,12 @@ app.get("/cart", async (req, res) => {
 });
 
 app.get("/account", async (req, res) => {
+    const category = await axios.get(base_url + '/category');
+    const account = await axios.get(base_url + '/users');
+    const customer = await axios.get(base_url + '/customers');
+
     try{
-        res.render("customer/account");
+        res.render("customer/account", { category: category.data });
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
@@ -95,10 +101,10 @@ app.get("/detail", async (req, res) => {
     try{
         const product = await axios.get(base_url + '/products');
         const category = await axios.get(base_url + '/category');
-        const brand = await axios.get(base_url + '/brands');
-        const productId = parseInt((req.query.id).substring(4), 10);
+        const brand = req.query.brand;
+        const Id = req.query.Id;
 
-        res.render("customer/productDetail", { products: product.data, category: category.data, brands: brand.data });
+        res.render("customer/productDetail", { products: product.data, category: category.data, brand, Id});
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
