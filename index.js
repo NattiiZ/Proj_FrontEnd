33344202b65f9113756872b4cddc14370d2a3865
NattiiZ <nat87972@gmail.com>
2025-03-04 12:20:16 +0700

    .

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const path = require("path");
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
 
 const api_port = process.env.API_PORT || 3000;
 const base_url = `http://localhost:${api_port}`;
-
-
 
 
 app.set("views" , path.join(__dirname, "/public/views"));
@@ -18,12 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(express.static(__dirname + '/public'));
-
-// const customerRoutes = express.Router();
-// const adminRoutes = express.Router();
-
-// app.use("/customer", customerRoutes);
-// app.use("/admin", adminRoutes);
 
 
 
@@ -34,7 +27,7 @@ app.get("/", async (req, res) => {
         const category = await axios.get(base_url + '/category');
         const brand = await axios.get(base_url + '/brands');
 
-        res.render("home", { products: product.data, category: category.data, brands: brand.data });
+        res.render("customer/home", { products: product.data, category: category.data, brands: brand.data });
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
@@ -43,22 +36,21 @@ app.get("/", async (req, res) => {
 
 
 app.get("/search", async (req, res) => {
-    const query = req.query.query;  // รับค่าจาก input ชื่อ query
+    const query = req.query.query;
     try {
-        const productList = await axios.get(base_url + '/products');  // ดึงสินค้าทั้งหมด
-        const categories = await axios.get(base_url + '/category');  // ดึงหมวดหมู่
+        const productList = await axios.get(base_url + '/products');
+        const categories = await axios.get(base_url + '/category');
         const brand = await axios.get(base_url + '/brands');
 
-        // ค้นหาสินค้าตามคำค้น
         const filteredProducts = productList.data.filter(product =>
-            product.name.toLowerCase().includes(query.toLowerCase())  // ค้นหาตามชื่อสินค้า
+            product.name.toLowerCase().includes(query.toLowerCase())
         );
 
-        res.render("products", {
+        res.render("customer/productSearch", {
             products: filteredProducts,
             category: categories.data,
             brands: brand.data,
-            query: query  // ส่ง query ที่ค้นหามาในหน้าผลลัพธ์
+            query: query
         });
     } catch (err) {
         console.error(err);
@@ -73,7 +65,7 @@ app.get("/login", async (req, res) => {
         const category = await axios.get(base_url + '/category');
         const brand = await axios.get(base_url + '/brands');
 
-        res.render("login", { products: product.data, category: category.data, brands: brand.data });
+        res.render("customer/login", { products: product.data, category: category.data, brands: brand.data });
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
@@ -87,7 +79,7 @@ app.get("/category", async (req, res) => {
         const category = await axios.get(base_url + '/category');
         const brand = await axios.get(base_url + '/brands');
 
-        res.render("category", { products: product.data, category: category.data, brands: brand.data });
+        res.render("customer/category", { products: product.data, category: category.data, brands: brand.data });
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
@@ -95,58 +87,41 @@ app.get("/category", async (req, res) => {
 });
 
 
-
-app.get("/book/:id", async (req, res) => {
+app.get("/cart", async (req, res) => {
     try{
-        const response = await axios.get(base_url + '/books/' + req.params.id);
-        res.render("book", { book: response.data });
+        const product = await axios.get(base_url + '/products');
+        const category = await axios.get(base_url + '/category');
+        const brand = await axios.get(base_url + '/brands');
+
+        res.render("customer/cart", { products: product.data, category: category.data, brands: brand.data });
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
-app.get("/create", (req, res) => {
-    res.render("create");
-});
 
-app.post("/create", async (req, res) => {
+app.get("/account", async (req, res) => {
     try{
-        const data = {title: req.body.title, author: req.body.author };
-        await axios.post(base_url + '/books', data);
-        res.redirect("/");
+        const product = await axios.get(base_url + '/products');
+        const category = await axios.get(base_url + '/category');
+        const brand = await axios.get(base_url + '/brands');
+
+        res.render("customer/account", { products: product.data, category: category.data, brands: brand.data });
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
     }
 });
 
-app.get("/update/:id", async (req, res) => {
-    try{
-        const response = await axios.get(
-        base_url + '/books/' + req.params.id);
-        res.render("update", { book: response.data });
-    }catch(err){
-        console.error(err);
-        res.status(500).send('Error');
-    }
-});
 
-app.post("/update/:id", async (req, res) => {
+app.get("/detail", async (req, res) => {
     try{
-        const data = {title: req.body.title, author: req.body.author };
-        await axios.put(base_url + '/books/' + req.params.id, data);
-        res.redirect("/");
-    }catch(err){
-        console.error(err);
-        res.status(500).send('Error');
-    }
-});
+        const product = await axios.get(base_url + '/products');
+        const category = await axios.get(base_url + '/category');
+        const brand = await axios.get(base_url + '/brands');
 
-app.get("/delete/:id", async (req, res) => {
-    try{
-        await axios.delete(base_url + '/books/' + req.params.id);
-            res.redirect("/");
+        res.render("customer/productDetail", { products: product.data, category: category.data, brands: brand.data });
     }catch(err){
         console.error(err);
         res.status(500).send('Error');
