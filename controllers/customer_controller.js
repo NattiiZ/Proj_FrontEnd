956535d0ec55ffd3708de0151e5214ce4873ea6f
NattiiZ <nat87972@gmail.com>
2025-03-07 12:52:42 +0700

    .

--- controllers/customer_controller.js
+++ controllers/customer_controller.js
@@ -1,29 +1,31 @@
 const axios = require('axios');
 const { check } = require('prettier');
 
-
 const base_url = `http://localhost:${process.env.API_PORT || 3000}`;
 
-
-
-
-exports.cart = async (req, res) => 
-{
+exports.cart = async (req, res) => {
     try {
         const loginSession = req.session.loginSession;
 
         if (!loginSession)
             return res.redirect(`/signin?from=${encodeURIComponent('/cart')}`)
 
-        const product = await axios.get(base_url + '/product');
         const category = await axios.get(base_url + '/category');
         const brand = await axios.get(base_url + '/brand');
+        const product = await axios.get(base_url + '/product');
+        const cart = await axios.get(base_url + '/cart');
+
+        const cartId = cart.data.find(cart => cart.user_ID == loginSession.UID);
+        
+        const cartItem = await axios.get(base_url + '/cart-item/' + cartId.cart_ID);
 
         res.render("customer/cart", { 
-            products: product.data, 
+            loginSession,
             category: category.data, 
             brands: brand.data,
-            loginSession
+            products: product.data, 
+            cartId: cartId.cart_ID,
+            cartItem: cartItem.data
         });
     }
     catch(err) {
@@ -32,43 +34,49 @@ exports.cart = async (req, res) =>
     }
 };
 
-exports.getProduct = async (req, res) => 
-{
+exports.getProduct = async (req, res) => {
     try {
         const loginSession = req.session.loginSession;
         const { url, Id } = req.body;
 
-        if (!loginSession)
+        if (!loginSession) {
             return res.redirect(`/signin?from=${encodeURIComponent(url)}`);
+        }
 
         const checkCart = await axios.get(base_url + '/cart');
-        const userCart = checkCart.data.find(cartItem => cartItem.user_ID === loginSession.UID);
+        let cart = checkCart.data.find(cartItem => cartItem.user_ID === loginSession.UID);
 
-        if (!userCart)
-            await axios.post(base_url + '/cart', { user_ID: loginSession.UID });
+        if (!cart) {
+            const newCart = await axios.post(base_url + '/cart', { user_ID: loginSession.UID });
+            cart = newCart.data;
+        }
 
-        const carts = await axios.get(base_url + '/cart');
-
-        const cart = carts.data.find(cart => cart.user_ID === loginSession.UID);
         const cartId = cart.cart_ID;
-        console.log("Cart_ID : " + cartId);
-        
 
-        const inCart = await axios.get(base_url + '/cart-item/' + cartId);
-        const findProduct = inCart.data.find(product => product.product_ID === Id)
+        const cartItems = await axios.get(base_url + '/cart-item');
+        const findItem = cartItems.data.find(item => item.cart_ID == cartId && item.product_ID == Id);
 
-        if (!findProduct) {
-            console.log("Go!!!");
-            
-            await axios.post(base_url + '/cart-item', { 
+        if (!findItem) {
+            await axios.post(base_url + '/cart-item', {
                 cart_ID: cartId,
                 product_ID: Id,
                 quantity: 1
             });
-        };
-    } 
-    catch (error) {
-        console.error(error);
+        } else
+            await axios.put(base_url + '/cart-item/' + findItem.cart_ID, { quantity: findItem.quantity + 1 });
+
+        const product = await axios.get(base_url + '/product/' + Id);
+        const brand = await axios.get(base_url + '/brand/' + product.data.brand_ID);
+
+        res.send(`
+            <script>
+                alert("เพิ่ม ${brand.data.name} ${product.data.name} ลงตะกร้าเรียบร้อยแล้ว");
+                window.location.href = "${url}";
+            </script>
+        `);
+    } catch (error) {
+        console.error("Error:", error);
         res.status(500).send('Error');
     }
 };
+
