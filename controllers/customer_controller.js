dc4b329cb1f98ca06affa3486ef7b7ca31a61a56
NattiiZ <nat87972@gmail.com>
2025-03-13 15:46:20 +0700

    .

--- controllers/customer_controller.js
+++ controllers/customer_controller.js
@@ -91,7 +91,7 @@ exports.getProduct = async (req, res) => {
         res.send(`
             <script>
                 alert("เพิ่ม ${brand.name} ${product.name} ลงตะกร้าเรียบร้อยแล้ว");
-                window.location.href = "http://localhost:${process.env.HOST_PORT}${url}";
+                window.location.href = "http://localhost${url}";
             </script>
         `);
     } 
